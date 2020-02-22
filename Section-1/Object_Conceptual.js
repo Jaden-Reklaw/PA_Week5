95d1fed929e2d2970fa401b23d8ce0e77c51a508
Jordan Walker <jwalker117@gmail.com>
2020-02-22 10:37:27 -0600

    adding week 5 video 1 notes

---
+++ Section-1/Object_Conceptual.js
@@ -0,0 +1,10 @@
+/*
+	Object Conceptual
+	Example of an object
+	A deck of cards and each individual card.
+	The number and the suit.
+	Accessed with the dot operator.
+	Objects have properties and values.
+	Cars have make, models, years, colors, etc...
+	Think of objects as this way.
+*/

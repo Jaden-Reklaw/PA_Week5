b0b56a1c1010e0aabd8f58e0101843e76526d2b4
Jordan Walker <jwalker117@gmail.com>
2020-02-22 17:34:43 -0600

    adding week 5 video 5 notes

---
+++ Section-3/Practical_Teamwork_GM.js
@@ -0,0 +1,51 @@
+/*
+	Practical Teamwork: Growth Mindset
+
+	Growth Mindset
+	The proxy for continuous learning.
+	Coined by Psycologist Carol Dweck.
+	
+	Opposite of a Growth Mindset
+	Fixed Mindset
+
+	What is a growth mindset?
+	The beleif that all traits can be cultivated to mastery through practice.
+	Work hard and smart to achieve great things.
+	Challenge yourself to become better everyday.
+
+	What is a fixed mindset?
+	The belief that intelligence is a static thing that can't be taught.
+	You either have it or you don't.
+	If you're smart you should have to try hard.
+	Myth created by the naturalists.
+
+	How fixed and growth mindsets see challenges?
+	Fixed Mindset: Confortable with the material.
+
+	Growth Mindset: If they know the material they will feel bored and 
+	challenge themselves with harder material. They don't like not learning.
+
+	Proximal Learning - More challenging task
+	Fixed Mindset is frustrated with Partial Win State
+	Growth Mindset Accepts challenges, Build New Skills
+
+	Extreme Challenges
+	Fixed mindset see the long road ahead that is not worth it.
+	Growth mindset see a break down of a fun journey.
+
+	Ask qustions when you don't know something but challenge yourself to try before
+	just asking questions.
+
+	Takeaways:
+	- Effort activates abilites.
+	- Setbacks and mistakes are a natural part of the learning process. It's what
+	happens when we take on valuable challenges.
+	- Recognize your effort and partial-wins when you engage in challenging work! Prazse
+	the process of diving into a tough problem.
+	
+
+
+
+
+
+*/

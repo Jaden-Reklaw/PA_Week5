b7c23f73c9f377209ae64db10ecff70e3f2a047b
Jordan Walker <jwalker117@gmail.com>
2020-02-22 15:51:27 -0600

    adding week 5 video 3 notes

---
+++ Section-2/Flow_Charting_Cond.js
@@ -0,0 +1,26 @@
+/*
+	Flow Charting 
+
+	Action in box
+	Decision in a diamond
+	
+	In decision are like using if statements
+	If (yes/true) do this is (no/false) do this.
+	Yes/true go down
+	No/false goes to the right
+
+	Case Study: Moving from house
+	Box full of cups from the kitchen
+	Max capcity 20 cups
+
+	Is box full?
+	true then close box
+	false then push cup object into the box
+
+	if(box.length >= 20) {
+		return false;
+	} else {
+		box.push(newCup);
+		return true;
+	}
+*/
